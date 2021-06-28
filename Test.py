20d6fb6e6c95f19cacd620a884155a4d5b8a44c9
Zhongzheng Qiao <ZQIAO002@e.ntu.edu.sg>
2021-06-28 11:42:22 +0800

    def Hello

---
+++ Test.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：learngit -> Test
+@IDE    ：PyCharm
+@Author ：Qiao Zhongzheng
+@Date   ：2021/6/28 11:31
+@Desc   ：
+=================================================='''
+
+def hello(name):
+    print(f'Hello, I am {name}')
